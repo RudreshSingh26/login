a26fb4660042e883bd172d8494f31ccf2d88d926
Rudresh Singh <singhrudresh7@gmail.com>
2022-08-29 18:19:57 +0530

    Added files and changes required for deployment to heroku

--- loginProject/settings.py
+++ loginProject/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&
 # DEBUG = True
 DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
-ALLOWED_HOSTS = ['http://rudresh.pythonanywhere.com/']
+ALLOWED_HOSTS = ['127.0.0.1']
 # ALLOWED_HOSTS = ['127.0.0.1','Rudresh.pythonanywhere.com']
 
 CRISPY_TEMPLATE_PACK = 'bootstrap3'
